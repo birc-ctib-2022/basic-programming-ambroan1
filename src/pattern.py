a70a0852c47be3480f2d3e706110862600adbc24
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-09-06 18:50:14 +0200

    I commit pattern

--- src/pattern.py
+++ src/pattern.py
@@ -1,4 +1,3 @@
-# Print the pattern
 for i in range (1,6):
     for j in range(0,i):
         if j == i-1:
@@ -6,11 +5,11 @@ for i in range (1,6):
         else:
             print('*', end=' ')
     print('')
-for i in range (5,1,-1):
-    for j in range(0,i-1):
+for i in range (4,0,-1):
+    for j in range(0,i):
         if j == i-1:
             print('*', end='')
         else:
             print('*', end=' ')
-    if i!= 2:
-        print('')
+    print('')
+    
