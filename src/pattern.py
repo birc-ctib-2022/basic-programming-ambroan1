1208a35cf0dd8f03c08c42a7eccc7e859ddf78fb
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-09-06 11:53:35 +0200

    I coded pattern

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,16 @@
 
 # Print the pattern
+for i in range (1,6):
+    for j in range(0,i):
+        if j == i-1:
+            print('*', end='')
+        else:
+            print('*', end=' ')
+    print('')
+for i in range (5,1,-1):
+    for j in range(0,i-1):
+        if j == i-1:
+            print('*', end='')
+        else:
+            print('*', end=' ')
+    print('')
