587b239aa6787d1836f4bc3778e9b4a8284c678c
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-09-06 11:37:37 +0200

    I coded print 1 to 10 growing

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,9 +1,11 @@
 
 # Print the numbers described in the exercise
-for i in range (1,12):
-    for j in range(1,i):
+for i in range (1,11):
+    for j in range(0,i):
         if j == i-1:
-            print(j, end='')
+            print(j+1, end='')
         else:
-            print(j, end=' ')
-    print('')
+            print(j+1, end=' ')
+    if i != 10:
+        print('')
+        
